3697bc0cd83c1fec0814ab7328a0dd287d6a9b93
Moisés <moisesmvuilo@gmail.com>
2026-02-07 20:42:33 +0000

    commit 4

---
+++ script.js
@@ -0,0 +1,47 @@
+document.getElementById('btn-carregar').addEventListener('click', () => {
+    fetch ('projetos.json') //Fazer requisição para arquivo JSON
+         .then(response => {
+            if (!response.ok) {
+                throw Error('Erro ao carregar os dados');
+            }
+            return response.json(); //Converter resposta para JSON
+         })    
+
+            .then(projetos => {
+                exibirProjetos(projetos); //Passar os dados para exibição
+            })
+            
+                .catch(error => {
+                    console.error('Erro:', error);
+                    alert('Não foi possível carregar os projetos');
+                });
+});
+
+function exibirProjetos() {
+
+    const lista = document.getElementById('lista-projetos');
+        lista.innerHTML = ''; //Limpar lista antes de adicionar novos projetos
+
+        projetos.forEach(projeto => {
+
+            //Criar a estrutura do projeto
+            const projetoDiv = document.getElementById('div');
+                projetoDiv.classList.add(projeto)
+
+                projetoDiv.classList.add(projeto.status.toLowerCase()); // Adicionar classe baseado no status
+
+                //Adicionar o nome do projeto
+                const titulo = document.createElement('h3');
+                titulo.textContent = projeto.nome;
+                projetoDiv.appendChild(titulo);
+
+                //Adicionar a descrição
+                const status = document.createElement('p');
+                status.textContent = `Status: ${projeto.status}`;
+                projetoDiv-appendChild(status);
+
+                //Adicionar o projeto à lista
+                lista.appendChild(projetoDiv)
+
+        })
+}
